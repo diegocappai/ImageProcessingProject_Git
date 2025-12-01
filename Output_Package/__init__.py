b5c9966db5decd98d1ce85450f24c9ee7d6e8598
diegocappai <diegocappai23@yahoo.it>
2025-12-01 11:55:08 +0100

    -

--- Output_Package/__init__.py
+++ Output_Package/__init__.py
@@ -4,7 +4,12 @@ from .zip_saver import ZipDataSetSaver
 def get_writer(method, output_path):
     if method == 'Cartella':
         return FolderDataSetSaver(output_path)
+
+    # TODO Decidere se implementare metodo Zip
+    """
     elif method == 'Zip':
         return ZipDataSetSaver(output_path)
+        """
+
     else:
         raise ValueError(f"Metodo {method} non valido")
